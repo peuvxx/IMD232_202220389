d0c1664e5dfaec5a8f0175ccb4f110f1882b8d9a
peuvxx <126849534+peuvxx@users.noreply.github.com>
2023-10-16 18:10:06 +0900

    7주차 수업

--- src/w5/assignment/step2/script/sketch.js
+++ src/w5/assignment/step2/script/sketch.js
@@ -4,7 +4,7 @@ let gravity;
 function setup() {
   setCanvasContainer('canvas', 2, 1, true);
 
-  pendulumA = new Pendulum(width / 2, 10, height / 3, (TAU / 360) * 45, 25);
+  pendulumA = new Pendulum(width / 2, 10, height / 2, (TAU / 360) * 45, 25);
   gravity = createVector(0, 0.5);
 
   background(255);
